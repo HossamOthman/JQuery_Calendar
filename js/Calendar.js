1449b54378adb5eaf50e3f0fee76ef9c1837c73d
Hossam Othman <rowheadrex@gmail.com>
2023-10-24 14:52:03 +0200

    show week

--- js/Calendar.js
+++ js/Calendar.js
@@ -1,11 +1,16 @@
+import { getDayIndex, addDays } from "./helper.js";
+
  export class Calendar {
     constructor() {
-
+        this.weekStart = null;
+        this.weekEnd = null;
     }
 
     setup() {
         this.setupTimes();
         this.setupDays();
+        this.calculateCurrentWeek();
+        this.showWeek();
     }
 
     setupTimes() {
@@ -26,6 +31,7 @@
             const dayIndex = parseInt($(this).attr('data-dayIndex'));
             const name = $(this).attr('data-name');
             const header = $("<div></div>").addClass("columnHeader").text(name);
+            $('<div></div>').addClass('dayDisplay').appendTo(header)
             const slots = $("<div></div>").addClass("slots");
         
             for (let hour = 0; hour < 24; hour++){
@@ -33,18 +39,45 @@
                 .addClass('slot')
                 .appendTo(slots)
                 .click(() => cal.clickSlot(hour, dayIndex))
-                .hover(() => cal.hoverOver(hour), () => cal.hoverOut());
+                .hover(
+                    () => cal.hoverOver(hour), 
+                    () => cal.hoverOut()
+                );
             }
             $(this).append(header).append(slots);
         })
     }
     clickSlot(hour, dayIndex) {
-        console.log('click', hour, day)
+        console.log('click', hour, dayIndex)
     }
     hoverOver(hour){
-        console.log('hovv', hour, dayIndex)
+        console.log('hovv', hour)
     }
     hoverOut() {
         // todo later
+    }
+    calculateCurrentWeek() {
+        const now = new Date();
+        this.weekStart = addDays(now, -getDayIndex(now));
+        this.weekEnd = addDays(this.weekStart, 6)
+    }
+
+    showWeek() {
+        const formatOptions = {
+            month: "2-digit",
+            day: "2-digit",
+            year: 'numeric',
+        };
+        $('#weekStartDisplay').text(this.weekStart.toLocaleDateString(undefined, formatOptions));
+        $('#weekEndDisplay').text(this.weekEnd.toLocaleDateString(undefined, formatOptions));
+
+        for (let dayIndex=0; dayIndex < 7; dayIndex++){
+            const date = addDays(this.weekStart, dayIndex);
+            const display = date.toLocaleDateString(undefined, {
+                month: "2-digit",
+                day: "2-digit",
+            });
+            $(`.day[data-dayIndex=${dayIndex}] .dayDisplay`).text(display);
+        }
     }
  }
